ecc63c6cea0d7bc568924021d997b0c2de9217cc
ASeomHan <hm9599@gmail.com>
2015-12-10 02:57:12 +0900

    Simplize region config
    Use region-id list, Remove owner/member type
    Remove info panel option

--- src/net/aseom/mc/wgregioninfo/Config.java
+++ src/net/aseom/mc/wgregioninfo/Config.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.List;
 import java.util.Set;
 
 import org.bukkit.configuration.InvalidConfigurationException;
@@ -18,17 +19,14 @@ public class Config {
 	private final String defaultRegionConfig
 		= "# Region Configuration\r\n"
 		+ "#\r\n"
-		+ "# filter-type은 ID, OWNER, MEMBER 세가지 값중 하나여야 합니다.\r\n"
-		+ "#\r\n"
 		+ "# Region ID가 'spawn' 또는 'shop'인 곳에 유저가 들어갈 때\r\n"
-		+ "# 정보 패널을 표시하지 않도록 하고, \"Welcome!\" 타이틀을 띄우고 싶다면\r\n"
-		+ "# 이렇게 설정하면 됩니다:\r\n"
+		+ "# \"Welcome!\" 타이틀을 띄우고 싶다면 이렇게 설정하면 됩니다.\r\n"
 		+ "\r\n"
-		+ "example:\r\n"
-		+ "  filter-type: RegionID\r\n"
-		+ "  filter: spawn\r\n"
-		+ "  greet-title: \"Welcome to Example Region!\"\r\n"
-		+ "  show-info-panel: true\r\n";
+		+ "example-rule:\r\n"
+		+ "  region-ids:\r\n"
+		+ "    - spawn\r\n"
+		+ "    - shop\r\n"
+		+ "  greet-title: \"Welcome!\"\r\n";
 	private File regionConfigFile;
 	public YamlConfiguration rgConf;
 	
@@ -40,10 +38,8 @@ public class Config {
 	public String getGreetTitle(ProtectedRegion region) {
 		Set<String> ruleIds = rgConf.getKeys(false);
 		for (String eachRule : ruleIds) {
-			String filterType = rgConf.getString(eachRule + ".filter-type");
-			String filter = rgConf.getString(eachRule + ".filter");
-			
-			if (filterType.equalsIgnoreCase("RegionID") && filter.equalsIgnoreCase(region.getId())) {
+			List<String> regionIdList = rgConf.getStringList(eachRule + ".region-ids");
+			if (regionIdList.contains(region.getId())) {
 				return rgConf.getString(eachRule + ".greet-title");
 			}
 		}
